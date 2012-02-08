a07a84c46616e6ffc56a6043174f01f56bfa6178
Bruno Wernimont <hello@brunowernimont.be>
2012-02-08 12:49:44 +0100

    update JSONKit

---
+++ Examples/CellMappingExample/CellMappingExample/CustomRowHeightTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  CustomRowHeightTableViewController.h
+//  CellMappingExample
+//
+//  Created by Bruno Wernimont on 8/02/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CustomRowHeightTableViewController : UITableViewController
+
+@end
---
+++ Examples/CellMappingExample/CellMappingExample/CustomRowHeightTableViewController.m
@@ -0,0 +1,13 @@
+//
+//  CustomRowHeightTableViewController.m
+//  CellMappingExample
+//
+//  Created by Bruno Wernimont on 8/02/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "CustomRowHeightTableViewController.h"
+
+@implementation CustomRowHeightTableViewController
+
+@end
