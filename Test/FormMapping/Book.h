9c4119b0f90022f7d6160682bc46e71d75093a7e
Bruno Wernimont <bruno.wernimont@gmail.com>
2012-05-20 06:46:18 +0200

    Add test for form mapping

---
+++ Test/FormMapping/Book.h
@@ -0,0 +1,16 @@
+#import <Foundation/Foundation.h>
+
+@interface Book : NSObject
+
+@property (nonatomic, retain) NSString *title;
+@property (nonatomic, retain) NSString *content;
+@property (nonatomic, retain) NSDate *releaseDate;
+@property (nonatomic, retain) NSNumber *numberOfActor;
+@property (nonatomic, retain) NSNumber *suitAllAges;
+@property (nonatomic, retain) NSString *password;
+@property (nonatomic, retain) NSString *shortName;
+@property (nonatomic, retain) NSString *choice;
+
++ (id)bookWithTitle:(NSString *)title content:(NSString *)content;
+
+@end
