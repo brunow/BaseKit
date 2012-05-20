0d6b00e17f00cc6b76bf059cb8fea919aca64efb
Bruno Wernimont <bruno.wernimont@gmail.com>
2012-05-20 06:55:27 +0200

    change project structure

---
+++ Resources/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  BaseKit
+//
+//  Created by Bruno Wernimont on 19/05/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
