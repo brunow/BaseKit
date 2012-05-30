179e967f9940f43136b6669f01a19db50d3c8456
Bruno Wernimont <bruno.wernimont@gmail.com>
2012-05-30 21:47:21 +0200

    [view] add test to show and hide view methods

--- Test/View/UIViewBaseKitSpecs.m
+++ Test/View/UIViewBaseKitSpecs.m
@@ -3,6 +3,11 @@
 SPEC_BEGIN(UIViewBaseKitSpecs)
 
 describe(@"UIView", ^{
+    __block UIView *_view;
+    
+    beforeEach(^{
+        _view = [[UIView alloc] init];
+    });
     
     it(@"should have textFields", ^{
         UIView *views = [[UIView alloc] init];
@@ -25,6 +30,16 @@ describe(@"UIView", ^{
         
         id firstTextField = [views findFirstTextField];
         [[firstTextField shouldNot] beNil];
+    });
+    
+    it(@"should hide view", ^{
+        [_view hide];
+        [[theValue(_view.alpha) should] equal:theValue(0)];
+    });
+    
+    it(@"should show view", ^{
+        [_view show];
+        [[theValue(_view.alpha) should] equal:theValue(1)];
     });
     
 });
