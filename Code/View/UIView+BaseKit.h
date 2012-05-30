b63409ecedb5cb5d487c2bad0908090464ddd176
Bruno Wernimont <bruno.wernimont@gmail.com>
2012-05-30 21:21:59 +0200

    [view] added categories to UIView show, hide, toggle, fadeInWithDuration:, fadeOutWithDuration: and fadeOutAndRemoveFromSuperviewWithDuration:

--- Code/View/UIView+BaseKit.h
+++ Code/View/UIView+BaseKit.h
@@ -25,4 +25,40 @@
 
 - (NSArray *)findTextFields;
 
+/**
+ Set alpha value of the receiver to 1
+ */
+- (void)show;
+
+/**
+ Set alpha value of the receiver to 0
+ */
+- (void)hide;
+
+/**
+ Toggle alpha value or the receiver between 0 and 1
+ */
+- (void)toggle;
+
+/**
+ Receiver will fade in
+ 
+ @param duration of the animation
+ */
+- (void)fadeInWithDuration:(NSTimeInterval)duration;
+
+/**
+ Receiver will fade out
+ 
+ @param duration of the animation
+ */
+- (void)fadeOutWithDuration:(NSTimeInterval)duration;
+
+/**
+ Receiver will fade out and remove the view from its superview after animation ended
+ 
+ @param duration of the animation
+ */
+- (void)fadeOutAndRemoveFromSuperviewWithDuration:(NSTimeInterval)duration;
+
 @end
